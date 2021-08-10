3496f2bf1739a30b5b3afa8ea27fc1687f8b4a0c
gabitheiss <t_gaaby@hotmail.com>
2021-08-10 14:59:12 -0300

    Trocando de fragments (telas) com click de botão

--- app/src/main/java/com/example/projetofragments/MainActivity.kt
+++ app/src/main/java/com/example/projetofragments/MainActivity.kt
@@ -2,24 +2,40 @@ package com.example.projetofragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.example.projetofragments.fragments.PrimeiroFragment
 import com.example.projetofragments.fragments.SegundoFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private var tipoFragment = TipoFragment.PRIMEIRO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container1, PrimeiroFragment())
-                .commitNow()
+           findViewById<Button>(R.id.buttonChangeFrag).apply{
+               setOnClickListener{
+                   if (tipoFragment == TipoFragment.PRIMEIRO){
+                       replaceFrag(PrimeiroFragment())
+                       tipoFragment = TipoFragment.SEGUNDO
+                   }else if(tipoFragment == TipoFragment.SEGUNDO){
+                       replaceFrag(SegundoFragment())
+                       tipoFragment = TipoFragment.PRIMEIRO
+                   }
+               }
+           }
 
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container2, SegundoFragment())
-            .commitNow()
-
-        
     }
+    private fun replaceFrag(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commitNow()
+    }
+}
+
+enum class TipoFragment{
+    PRIMEIRO,
+    SEGUNDO
 }
